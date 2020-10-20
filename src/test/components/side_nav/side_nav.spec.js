e27326aef990857895ecf4eda843e0562292b596
cool-yue <379175365@qq.com>
2020-10-20 17:31:19 +0800

    添加side-nav的测试case

--- src/test/components/side_nav/side_nav.spec.js
+++ src/test/components/side_nav/side_nav.spec.js
@@ -34,4 +34,53 @@ describe("sideNav", () => {
             expect(item.textContent).to.equal(`菜单${i}`);
         });
     });
+    it("点击某个项目能够高亮,并且触发on-click-item事件,并且传入当前tab的名称", () => {
+        const constructor = Vue.extend(SideNav);
+        const items = [
+            {
+                title: "菜单0",
+                children: []
+            },
+            {
+                title: "菜单1",
+                children: []
+            },
+            {
+                title: "菜单2",
+                children: []
+            }
+        ];
+        const vm = new constructor({
+            propsData: {
+                items
+            }
+        });
+
+        vm.$mount();
+        const cb = sinon.fake();
+        vm.$on("on-click-item", cb);
+        const dom = vm.$el;
+        const selector = ".side-nav__item";
+        const domItem_first = dom.querySelector(selector);
+        const domItem_second = dom.querySelector(selector + ":nth-child(2)");
+        const domItem_last = dom.querySelector(selector + ":last-child");
+        domItem_last.click();
+
+        expect(cb.calledOnce).to.be.true;
+        expect(cb.calledWith("菜单2")).to.be.true;
+        Vue.nextTick(function () {
+            expect(domItem_first.classList.contains(".side-nav__item--active")).to.be.false;
+            expect(domItem_second.classList.contains(".side-nav__item--active")).to.be.false;
+            expect(domItem_last.classList.contains(".side-nav__item--active")).to.be.true;
+        });
+
+        domItem_second.click();
+        expect(cb.calledOnce).to.be.true;
+        expect(cb.calledWith("菜单1")).to.be.true;
+        Vue.nextTick(function () {
+            expect(domItem_first.classList.contains(".side-nav__item--active")).to.be.false;
+            expect(domItem_second.classList.contains(".side-nav__item--active")).to.be.true;
+            expect(domItem_last.classList.contains(".side-nav__item--active")).to.be.false;
+        });
+    });
 });
